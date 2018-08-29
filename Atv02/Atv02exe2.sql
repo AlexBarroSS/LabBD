3051577b210ea9ea9f6ab34b5f7ad2d5264d871f
Alex Barros <alex.silva123@fatec.sp.gov.br>
2018-08-28 21:38:04 -0300

    Table Item Pedido

--- Atv02/Atv02exe2.sql
+++ Atv02/Atv02exe2.sql
@@ -18,6 +18,24 @@ INSERT INTO Cliente (cod_cliente, nome_cliente, endereco, cidade, cep, estado)
 VALUES ('00004', 'Barbw', 'Campolinho', 'Votorantim', '18184-000', 'sp' );
 
 /*
+Tabela Vendedor
+*/
+INSERT INTO Vendedor (cod_vendedor, nome_vendedor, faixa_comissao, salario_fixo)
+VALUES ('10001', 'Uno', '15,20', '11231,21');
+
+INSERT INTO Vendedor (cod_vendedor, nome_vendedor, faixa_comissao, salario_fixo)
+VALUES ('10002', 'Vender Dor', '25,5', '7500,50');
+
+INSERT INTO Vendedor (cod_vendedor, nome_vendedor, faixa_comissao, salario_fixo)
+VALUES ('10003', 'Comen ciante', '13,17', '12345,12');
+
+INSERT INTO Vendedor (cod_vendedor, nome_vendedor, faixa_comissao, salario_fixo)
+VALUES ('10004', 'Jose Amendoin', '30,30', '3030,30');
+
+INSERT INTO Vendedor (cod_vendedor, nome_vendedor, faixa_comissao, salario_fixo)
+VALUES ('10005', 'Alex Afonso de B Silva', '10,23', '12500,43');
+
+/*
 Tabela Produto
 */
 
@@ -37,25 +55,42 @@ INSERT INTO Produto (cod_produto, descricao, unidade, valor_unitario)
 VALUES ('105', 'caixa de laranja', 'kg', '3,05' );
 
 /*
-Tabela Vendedor
+Tabela Pedido
 */
-INSERT INTO Vendedor (cod_vendedor, nome_vendedor, faixa_comissao, salario_fixo)
-VALUES ('10001', 'Uno', '15,20', '11231,21');
 
-INSERT INTO Vendedor (cod_vendedor, nome_vendedor, faixa_comissao, salario_fixo)
-VALUES ('10002', 'Vender Dor', '25,5', '7500,50');
+INSERT INTO Pedido (num_pedido, prazo_entrega, cod_cliente, cod_vendedor)
+VALUES('301', '11-JAN-1997', '00001', '10001');
 
-INSERT INTO Vendedor (cod_vendedor, nome_vendedor, faixa_comissao, salario_fixo)
-VALUES ('10003', 'Comen ciante', '13,17', '12345,12');
+INSERT INTO Pedido (num_pedido, prazo_entrega, cod_cliente, cod_vendedor)
+VALUES('302', '11-JAN-2007', '00001', '10001');
 
-INSERT INTO Vendedor (cod_vendedor, nome_vendedor, faixa_comissao, salario_fixo)
-VALUES ('10004', 'Jose Amendoin', '30,30', '3030,30');
+INSERT INTO Pedido (num_pedido, prazo_entrega, cod_cliente, cod_vendedor)
+VALUES('303', '11-JAN-2008', '00002', '10001');
 
-INSERT INTO Vendedor (cod_vendedor, nome_vendedor, faixa_comissao, salario_fixo)
-VALUES ('10005', 'Alex Afonso de B Silva', '10,23', '12500,43');
+INSERT INTO Pedido (num_pedido, prazo_entrega, cod_cliente, cod_vendedor)
+VALUES('304', '11-JAN-2009', '00003', '10002');
 
+INSERT INTO Pedido (num_pedido, prazo_entrega, cod_cliente, cod_vendedor)
+VALUES ('305', '11-JAN-2009', '00001', '10005');
 
+/*
+Tabela Item Pedido
+*/
 
+INSERT INTO Item_Pedido(cod_produto, num_pedido, quantidade)
+VALUES ('105','305','12345');
+
+INSERT INTO Item_Pedido(cod_produto, num_pedido, quantidade)
+VALUES('104','304','2');
+
+INSERT INTO Item_Pedido(cod_produto, num_pedido, quantidade)
+VALUES('102','303','4');
+
+INSERT INTO Item_Pedido(cod_produto, num_pedido, quantidade)
+VALUES('102','302','8');
+
+INSERT INTO Item_Pedido(cod_produto, num_pedido, quantidade)
+VALUES('101','301','7');
 
 
 
