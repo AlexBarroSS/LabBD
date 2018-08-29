e9c3cf9e370a82664b7d300c47341562b167a82f
Alex Barros <alex.silva123@fatec.sp.gov.br>
2018-08-28 21:15:39 -0300

    Insert Produto

---
+++ Atv02/Atv02exe2.sql
@@ -0,0 +1,46 @@
+/*
+Tabela Cliente
+*/
+
+INSERT INTO Cliente (cod_cliente, nome_cliente, endereco, cidade, cep, estado)
+VALUES ('00001', 'Alex Afonso', 'Campestre', 'Pilar do Sul', '18185-000', 'sp');
+
+INSERT INTO Cliente (cod_cliente, nome_cliente, endereco, cidade, cep, estado)
+VALUES ('00002', 'Guxtavo', 'Centro', 'Sorocaba', '18181-000', 'sp' );
+
+INSERT INTO Cliente (cod_cliente, nome_cliente, endereco, cidade, cep, estado)
+VALUES ('00003', 'Andure', 'Vargem grande', 'Paulista', '18183-000', 'sp');
+
+INSERT INTO Cliente (cod_cliente, nome_cliente, endereco, cidade, cep, estado)
+VALUES ('00005', 'Guilhereme', 'Olimpo', 'Grecia', '18123-000', 'sp' );
+
+INSERT INTO Cliente (cod_cliente, nome_cliente, endereco, cidade, cep, estado)
+VALUES ('00004', 'Barbw', 'Campolinho', 'Votorantim', '18184-000', 'sp' );
+
+/*
+Tabela Produto
+*/
+
+INSERT INTO Produto (cod_produto, descricao, unidade, valor_unitario)
+VALUES ('101', 'caixa de sapato', 'kg', '100,50' ); 
+
+INSERT INTO Produto (cod_produto, descricao, unidade, valor_unitario)
+VALUES ('102', 'caixa de doce', 'kg', '101,50' );
+
+INSERT INTO Produto (cod_produto, descricao, unidade, valor_unitario)
+VALUES ('103', 'caixa de algodao', 'kg', '210,54' );
+
+INSERT INTO Produto (cod_produto, descricao, unidade, valor_unitario)
+VALUES ('104', 'café', 'kg', '191210,50' );
+
+INSERT INTO Produto (cod_produto, descricao, unidade, valor_unitario)
+VALUES ('105', 'caixa de laranja', 'kg', '3,05' );
+
+
+
+
+
+
+
+
+
