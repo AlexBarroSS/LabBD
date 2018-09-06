f68abd7375711eab14e237b33a5727c8c7fbeeec
Alex Barros <alex.silva123@fatec.sp.gov.br>
2018-09-06 01:02:08 -0300

    Atividade LBD 06/09

--- Atv04/ATIVIDADE_06_09.sql
+++ Atv04/ATIVIDADE_06_09.sql
@@ -1,3 +1,41 @@
+/*
+ATIVIDADE 2.5 E 2.6
+
+*/
+
+--VIEW
+
+--exe 01
+
+CREATE OR REPLACE VIEW view_pedido
+AS SELECT num_pedido, cod_cliente, prazo_entrega
+FROM pedido   
+
+--exe 02
+
+CREATE OR REPLACE VIEW view_produto
+AS SELECT *
+FROM produto
+WHERE unidade = 'kg'  
+
+--exe 03
+
+CREATE OR REPLACE VIEW view_valor
+AS SELECT * 
+FROM produto
+WHERE valor_unitario < ( SELECT  AVG(valor_unitario) FROM produto)
+
+--exe 04
+
+CREATE OR REPLACE VIEW view_valor
+AS SELECT v.cod_vendedor, v.nome_vendedor, COUNT(p.cod_vendedor) as Qtde_Vendas
+FROM vendedor v
+INNER JOIN pedido p
+ON p.cod_vendedor = v.cod_vendedor
+GROUP BY  v.cod_vendedor, v.nome_vendedor;
+
+--SUBCONSULTAS
+
 -- exe 01
 
 SELECT cidade
