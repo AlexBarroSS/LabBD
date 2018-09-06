3a2214c09dfd2b064340c70b91ac3337237f1894
Alex Barros <alex.silva123@fatec.sp.gov.br>
2018-09-05 23:01:16 -0300

    Ultimas mudanças atv04
    view
    exe01
    sub

--- Atv04/atv04sub.sql
+++ Atv04/atv04sub.sql
@@ -11,3 +11,23 @@ FROM produto
 WHERE valor_unitario > ( SELECT  AVG(valor_unitario) FROM produto);
 
 -- exe 03
+
+SELECT c.cod_cliente, c.nome_cliente
+FROM cliente c
+INNER JOIN pedido p
+ON c.cod_cliente = p.cod_cliente
+WHERE p.cod_vendedor NOT IN (SELECT cod_vendedor FROM Pedido WHERE cod_vendedor <> 10)
+
+--exe 04
+
+ SELECT pedido.cod_vendedor, nome_vendedor
+ FROM pedido,vendedor
+ WHERE pedido.cod_vendedor = vendedor.cod_vendedor
+ GROUP BY pedido.cod_vendedor, nome_vendedor
+ HAVING count(*) <= 3;
+
+ -- 05
+
+ 
+
+
--- Atv04/atv04view.sql
+++ Atv04/atv04view.sql
@@ -1,47 +1,45 @@
--- exe 2.3
+--patterns view_name_view
 
--- exe 01
+--exe 01
 
-SELECT MAX(valor_unitario) AS Maior_valor
+CREATE OR REPLACE VIEW view_pedido
+AS SELECT num_pedido, cod_cliente, prazo_entrega
+FROM pedido   
+
+--exe 02
+
+CREATE OR REPLACE VIEW view_produto
+AS SELECT *
 FROM produto
+WHERE unidade = 'kg'  
 
--- exe 02
+--exe 03
 
-SELECT MAX(valor_unitario) AS Maior_valor
+CREATE OR REPLACE VIEW view_valor
+AS SELECT * 
 FROM produto
+WHERE valor_unitario < ( SELECT  AVG(valor_unitario) FROM produto)
 
--- exe 03
+--exe 04
 
-SELECT SUM(salario_fixo) AS Soma_salario
-FROM vendedor
-
--- exe 04
-
-SELECT COUNT(unidade) AS Unit
-FROM produto
-WHERE unidade = 'Lt'
+CREATE OR REPLACE VIEW view_valor
+AS SELECT v.cod_vendedor, v.nome_vendedor, COUNT(p.cod_vendedor) as Qtde_Vendas
+FROM vendedor v
+INNER JOIN pedido p
+ON p.cod_vendedor = v.cod_vendedor
+GROUP BY  v.cod_vendedor, v.nome_vendedor;
 
 -- exe 05
 
-SELECT COUNT(*) AS Cont_cod
-FROM cliente
-GROUP BY cidade;
+SELECT MAX(quantidade) AS Maior
+FROM item_pedido
 
--- exe 06
 
-SELECT COUNT(*) AS Pedido_vendas
-FROM vendedor v, pedido p
-WHERE v.cod_vendedor = p.cod_vendedor
 
--- exe 07
+--exe 06
 
-SELECT MAX(valor_unitario) AS maior_unit, MIN(valor_unitario) AS menor_unit
-FROM produto
-GROUP BY unidade;
-
--- exe 08
-
-SELECT COUNT(*) AS numero_clientes
-FROM cliente
-GROUP BY cidade
-HAVING COUNT(*) > 4;
+SELECT c.nome_cliente, c.endereco
+FROM cliente c
+INNER JOIN pedido pe
+ON c.cod_cliente = pe.cod_cliente
+WHERE c.cidade = 'Itu'OR c.cidade = 'Sorocaba' AND pe.cod_vendedor = '10'
