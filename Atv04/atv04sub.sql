c01a2bbc2c0aef156d1215cdd36d2199914bdc4d
Alex Barros <alex.silva123@fatec.sp.gov.br>
2018-09-05 23:38:47 -0300

    FOI OQUE DEU

--- Atv04/atv04sub.sql
+++ Atv04/atv04sub.sql
@@ -16,7 +16,11 @@ SELECT c.cod_cliente, c.nome_cliente
 FROM cliente c
 INNER JOIN pedido p
 ON c.cod_cliente = p.cod_cliente
-WHERE p.cod_vendedor NOT IN (SELECT cod_vendedor FROM Pedido WHERE cod_vendedor <> 10)
+WHERE p.cod_vendedor 
+NOT IN (
+SELECT cod_vendedor 
+FROM Pedido 
+WHERE cod_vendedor <> 10)
 
 --exe 04
 
@@ -24,10 +28,28 @@ WHERE p.cod_vendedor NOT IN (SELECT cod_vendedor FROM Pedido WHERE cod_vendedor 
  FROM pedido,vendedor
  WHERE pedido.cod_vendedor = vendedor.cod_vendedor
  GROUP BY pedido.cod_vendedor, nome_vendedor
- HAVING count(*) <= 3;
+ HAVING count(*) <= 5;
 
- -- 05
+ --exe 05
 
- 
+SELECT nome_vendedor 
+FROM vendedor
+WHERE cod_vendedor 
+NOT IN (
+SELECT cod_vendedor 
+FROM pedido
+WHERE to_char(prazo_entrega,'mm/yyyy') = '05/2017');
 
+ --exe 06
 
+SELECT nome_vendedor
+FROM Pedido p
+WHERE p.nome_vendedor 
+IN(
+SELECT nome_vendedor
+FROM vendedor
+)
+
+ --exe 08
+
+ DELETE
