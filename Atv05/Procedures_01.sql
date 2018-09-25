11606bf2a341a6e8d328425756417d888392e3a4
AlexBarroSS <alex.silva123@fatec.sp.gov.br>
2018-09-25 16:43:55 -0300

    Correcao

--- Atv05/Procedures_01.sql
+++ Atv05/Procedures_01.sql
@@ -2,13 +2,15 @@ create or replace procedure cont_pedidos(pcodcli number)
 as
     tot_ped number;
 Begin
-    select cod_cliente into tot_ped
+    select count(cod_cliente) into tot_ped
     from pedido where cod_cliente = pcodcli;
 
     if tot_ped is not null then
         select count(cod_cliente) into tot_ped from pedido where cod_cliente = pcodcli; 
         insert into tab_mensagem
-    values (sysdate, 'Cliente ' || pcodcli, ' tem ' || tot_ped);
+        values (sysdate, 'Cliente ' || pcodcli, ' tem ' || tot_ped); 
+    else
+       insert into tberro values (sysdate,' Deu ruim' || pcodcli);
    end if;
 
 commit;
