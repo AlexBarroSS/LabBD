c55f50a040dc8eb59af2a789d15eca8eb4d37a3a
AlexBarroSS <alex.silva123@fatec.sp.gov.br>
2018-09-26 15:34:39 -0300

    Procedures trabalho dia 20-09 VF

--- Atv05/Procedures_01.sql
+++ Atv05/Procedures_01.sql
@@ -24,3 +24,28 @@ select * from tberro;
 select * from tab_mensagem;
 select * from pedido;
 select * from cliente;
+
+CREATE OR REPLACE procedure cont_pedidos(p_codcli NUMBER)
+AS 
+    v_tot_ped NUMBER;
+    v_nome_cli cliente.nome_cliente%TYPE;
+BEGIN
+    SELECT cod_cliente INTO v_tot_ped
+    FROM cliente WHERE cod_cliente = p_codcli;
+    
+    SELECT coalesce (count(cod_cliente), 0) INTO v_tot_ped 
+    FROM pedido WHERE cod_cliente = p_codcli;
+
+    SELECT nome_cliente INTO v_nome_cli 
+    FROM cliente WHERE cod_cliente = p_codcli;
+    
+    INSERT INTO tab_mensagem
+    VALUES (SYSDATE, 'Cliente ' || p_codcli, v_nome_cli ||' tem ' || v_tot_ped); 
+    
+COMMIT;
+
+EXCEPTION
+    WHEN no_data_found THEN
+      INSERT INTO tberro VALUES (SYSDATE,' Cliente nao existente ' || p_codcli);
+
+END;
