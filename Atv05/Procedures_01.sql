5c8087e150b566fc01e1e56e54c8941ec7e9569a
AlexBarroSS <alex.silva123@fatec.sp.gov.br>
2018-09-27 10:32:33 -0300

    Modify

--- Atv05/Procedures_01.sql
+++ Atv05/Procedures_01.sql
@@ -1,24 +1,3 @@
-create or replace procedure cont_pedidos(pcodcli number)
-as
-    v_tot_ped number;
-    v_nome_cli cliente.nome_cliente%type;
-Begin
-    select cod_cliente into v_tot_ped
-    from cliente where cod_cliente = pcodcli;
-    
-    select coalesce (count(cod_cliente), 0) into v_tot_ped from pedido where cod_cliente = pcodcli;
-    select nome_cliente into v_nome_cli from cliente where cod_cliente = pcodcli;
-    insert into tab_mensagem
-    values (sysdate, 'Cliente ' || pcodcli, v_nome_cli ||' tem ' || v_tot_ped); 
-    
-commit;
-
-exception
-    when no_data_found then
-      insert into tberro values (sysdate,' Cliente nao existente ' || pcodcli);
-
-end;
-
 exec cont_pedidos(34567);
 select * from tberro;
 select * from tab_mensagem;
@@ -30,10 +9,8 @@ AS
     v_tot_ped NUMBER;
     v_nome_cli cliente.nome_cliente%TYPE;
 BEGIN
-    SELECT cod_cliente INTO v_tot_ped
-    FROM cliente WHERE cod_cliente = p_codcli;
-    
-    SELECT coalesce (count(cod_cliente), 0) INTO v_tot_ped 
+   
+    SELECT count(cod_cliente) INTO v_tot_ped 
     FROM pedido WHERE cod_cliente = p_codcli;
 
     SELECT nome_cliente INTO v_nome_cli 
