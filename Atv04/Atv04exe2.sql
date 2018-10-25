a7ef761500fccd94402a5fc589360326a8cd3e16
AlexBarroSS <alex.silva123@fatec.sp.gov.br>
2018-10-25 10:14:08 -0300

    Exe04

--- Atv04/Atv04exe2.sql
+++ Atv04/Atv04exe2.sql
@@ -1,5 +1,7 @@
 //exe01
 
+
+
 //exe02
 
 //exe03
@@ -22,12 +24,7 @@ BEGIN
     IF v_tot_ped = 0 THEN
     
         INSERT INTO tablog
-        VALUES(
-            SYSDATE,
-            p_codpro || ' ',
-            v_descricao
-        );
-
+        VALUES( SYSDATE, p_codpro || ' ', v_descricao);
         DELETE produto
         WHERE cod_produto = p_codpro;
 
@@ -38,21 +35,3 @@ EXCEPTION
       INSERT INTO tberro VALUES (SYSDATE, ' Produto nao encontrado ' || p_codpro );
 
 END;
-
-create or replace procedure veri_prod(p_cod_prod number)
-AS  
-    v_total number(10);
-    v_desc Produto.descricao%type;
-Begin
-    select descricao into v_desc from Produto where cod_produto = p_cod_prod;
-    select count(*) into v_total from Item_Pedido where cod_produto = p_cod_prod;
-    if v_total = 0 then
-        insert into values (sysdate, P_cod_prod || ' ' || v_desc, user );
-        delete Produto where cod_produto = P_cod_prod;
-    end if;
-
-EXCEPTION
-    when no_data_found then
-        insert into tberro values( sysdate, 'Produto nao existe' || P_cod_prod);
-
-end;
